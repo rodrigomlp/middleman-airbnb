100d3ff3b2526e6703b39763f030b3cb40ee15f2
Boris Paillard <boris.paillard@gmail.com>
2016-01-31 15:11:44 +0100

    adding dynamic show view

--- config.rb
+++ config.rb
@@ -31,6 +31,10 @@ activate :autoprefixer
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/show.html", :locals => { :owner => name }, :ignore => true
+end
+
 ###
 # Helpers
 ###
